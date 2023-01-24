2343ec86ec79af7d34d040ded2b3c392bac6d124
Jithub0411 <hoonjh6689@gmail.com>
2023-01-24 22:16:21 +0900

    2023.1.24 intelliJ

---
+++ Baekjoon_Practice/src/BaeJ_1712.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class BaeJ_1712 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        // a = 고정비용, b = 가변 비용
+        int a,b;
+        // c = 가격
+        int c;
+        // 총 수입
+
+        a = in.nextInt();
+        b = in.nextInt();
+        c = in.nextInt();
+
+        // 이익이 발생하지 않을 시
+       if (c <= b) {
+           System.out.println("-1");
+       }
+       // 손익분기점 계산
+       else {
+           System.out.println(a/(c-b)+1);
+       }
+
+    }
+}
